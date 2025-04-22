d9b1e73a75dfdd2cbbadb2030045d90acb1e462d
yamaniyuda <yamaniyuda@gmail.com>
2025-04-22 18:01:17 +0700

    install provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,21 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:flutter/services.dart';
 import 'package:one_goal/app/config/routes.dart';
 import 'package:one_goal/app/config/theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
     ));
     usePathUrlStrategy();
-    runApp(const MyApp());
+    runApp(
+		MultiProvider(
+			providers: [
+				// Add your providers here
+			],
+			child: const MyApp(),
+		)
+	);
 }
 
 class MyApp extends StatelessWidget {
