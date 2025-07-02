eeccfe6cdf39636e9deeb1e051b0344e3c44d1df
fela <fela@gmail.com>
2025-07-03 00:06:25 +0700

    add card in dashboard beranda

--- lib/presentation/providers/localizations_provider.dart
+++ lib/presentation/providers/localizations_provider.dart
@@ -2,21 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalizationsProvider with ChangeNotifier {
-  Locale _locale = const Locale('en');
+    Locale _locale = const Locale('en');
 
-  Locale get locale => _locale;
+    Locale get locale => _locale;
 
-  Future<void> loadLocale() async {
-    final prefs = await SharedPreferences.getInstance();
-    final localeCode = prefs.getString('locale') ?? 'en';
-    _locale = Locale(localeCode);
-    notifyListeners();
-  }
+    Future<void> loadLocale() async {
+        final prefs = await SharedPreferences.getInstance();
+        final localeCode = prefs.getString('locale') ?? 'en';
+        _locale = Locale(localeCode);
+        notifyListeners();
+    }
 
-  Future<void> setLocale(String languageCode) async {
-    _locale = Locale(languageCode);
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('locale', languageCode);
-    notifyListeners();
-  }
+    Future<void> setLocale(String languageCode) async {
+        _locale = Locale(languageCode);
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('locale', languageCode);
+        notifyListeners();
+    }
 }
